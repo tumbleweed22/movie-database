30f6dda24607c239b98ca447d6dfad516198d4f8
tumbleweed22 <a.hooper22@outlook.com>
2025-08-17 12:53:09 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const axios = require('axios');
